c49a4d93cf945292a4f01fc49ef43a334e94e977
wang <wangdoje@gmail.com>
2024-04-28 16:55:21 +0530

    title updated

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
 
-    <h1>MLOps & LLMOps from Psitron</h1>
+    <h1>MLOps & LLMOps</h1>
 	<h2>Upload your data to get Gen Ai predictions</h2>
     <img
       alt="Vue logo"
